1b33cfb320a60444880f6dc4708fc36fe0b67592
Sajith Bandara <sajith.bandara2@gmail.com>
2022-06-10 15:33:44 -0500

    added hosted fields to new action

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -8,10 +8,16 @@ class CheckoutsController < ApplicationController
   def gateway
     gateway = Braintree::Gateway.new(
       :environment => :sandbox,
-      :merchant_id => "use_your_merchant_id",
-      :public_key => "use_your_public_key",
-      :private_key => "use_your_private_key",
+      :merchant_id => "fs5wp6qdc238xcwf",
+      :public_key => "5gz8f2yhmcy3hj4r",
+      :private_key => "c07e7d8b020484b8739f2cd688604129",
     )
   end
 
+  # pass client_token to your front-end
+  def new
+    @client_token = gateway.client_token.generate(
+    #:customer_id => a_customer_id
+    )
+  end
 end
