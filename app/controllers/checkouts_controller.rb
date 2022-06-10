72f7f9b1bd638914ec035084dd991756118fd9e9
Sajith Bandara <sajith.bandara2@gmail.com>
2022-06-10 12:51:05 -0500

    added API credentials and client_token.generate to checkouts_controller

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,3 +1,17 @@
 class CheckoutsController < ApplicationController
 
+  def show
+
+  end
+
+  # enter API credentials below
+  def gateway
+    gateway = Braintree::Gateway.new(
+      :environment => :sandbox,
+      :merchant_id => "use_your_merchant_id",
+      :public_key => "use_your_public_key",
+      :private_key => "use_your_private_key",
+    )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :checkouts
+  resources :checkouts, only: [:show, :index, :new]
 end
